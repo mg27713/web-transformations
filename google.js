2a51259246f24c1d7d25fed7d6c86e837287e47c
mg27713 <68036487+mg27713@users.noreply.github.com>
2020-10-13 17:51:03 -0700

    Add the very important Google transformation.

---
+++ google.js
@@ -0,0 +1,39 @@
+// addEventListener("load", () => {
+//     // alert("running!");
+    
+//     antiGoogleStuff();
+// });
+
+function antiGoogleStuff() {
+    // let uglyLogo = document.getElementById("hplogo");
+    
+    // if (uglyLogo !== null) {
+    //     uglyLogo.src = "";
+    //     alert("replaced?");
+    // } else if (confirm("Continue?"))
+    //     requestAnimationFrame(antiGoogleStuff);
+    
+    //for (let search of document.getElementsByTagName("input")) {
+    let search = document.getElementsByTagName("input")[5];
+        search.style.display = "none";
+    search.parentElement.parentElement.parentElement.style.visibility = "hidden";
+
+        
+        try {
+            let placeholder = document.createElement("div");
+            placeholder.innerHTML = 'Google Search is prohibited on this device. Did you mean to go to <a href="https://duckduckgo.com">DuckDuckGo</a>?';
+            placeholder.style.visibility = "visible";
+            placeholder.style.textAlign = "center";
+            search.parentElement.insertBefore(placeholder, search);
+        } catch (e) {
+            alert(e);
+            return;
+        }
+    
+    for (let input of document.getElementsByTagName("input"))
+        if (input.type == "submit")
+            input.style.display = "none";
+    //}
+}
+
+addEventListener("load", antiGoogleStuff);
